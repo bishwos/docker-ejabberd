8693a5423979943a6450753e14a5f5b9d3f4a8b3
Bishwo Shrestha <bishwo.pro@gmail.com>
2022-09-21 22:22:42 +0545

    Multiple log files

--- docker-ejabberd/ecs/conf/ejabberd_python_auth.py
+++ docker-ejabberd/ecs/conf/ejabberd_python_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from pathlib import Path
 from struct import *
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
@@ -8,11 +9,36 @@ from configparser import ConfigParser
 import json
 import logging
 
+
+def is_file_in_use(file_path):
+    path = Path(file_path)
+
+    if not path.exists():
+        raise FileNotFoundError
+
+    try:
+        path.rename(path)
+    except PermissionError:
+        return True
+    else:
+        return False
+
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-file_handler = logging.FileHandler('/home/ejabberd/logs/logs.log')
+logFileName = 'logs5.log'
+if not is_file_in_use('/home/ejabberd/logs/logs.log'):
+    logFileName = '/home/ejabberd/logs/logs.log'
+if not is_file_in_use('/home/ejabberd/logs/logs2.log'):
+    logFileName = '/home/ejabberd/logs/logs2.log'
+if not is_file_in_use('/home/ejabberd/logs/logs3.log'):
+    logFileName = '/home/ejabberd/logs/logs3.log'
+if not is_file_in_use('/home/ejabberd/logs/logs4.log'):
+    logFileName = '/home/ejabberd/logs/logs4.log'
+
+file_handler = logging.FileHandler(logFileName)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -20,6 +46,7 @@ logger.addHandler(file_handler)
 config = ConfigParser()
 config.read('.env')
 logging.info('started')
+
 
 
 def do_auth(args):
