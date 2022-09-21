48aba2c211ee5816e51d18f136f46b27fab9a5b5
Bishwo Shrestha <bishwo.pro@gmail.com>
2022-09-21 22:02:18 +0545

    Fix logging path

--- docker-ejabberd/ecs/conf/ejabberd_python_auth.py
+++ docker-ejabberd/ecs/conf/ejabberd_python_auth.py
@@ -12,7 +12,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-file_handler = logging.FileHandler('logs.log')
+file_handler = logging.FileHandler('/home/ejabberd/logs/logs.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
