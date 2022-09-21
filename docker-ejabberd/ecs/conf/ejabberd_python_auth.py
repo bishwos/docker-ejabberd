272e89bce56c51d4247592206c7d52ea19234d51
Bishwo Shrestha <bishwo.pro@gmail.com>
2022-09-21 21:53:34 +0545

    External auth config

--- docker-ejabberd/ecs/conf/ejabberd_python_auth.py
+++ docker-ejabberd/ecs/conf/ejabberd_python_auth.py
@@ -25,12 +25,15 @@ def do_auth(args):
     (username, server, token) = args
     if username == 'admin' and token == 'asd':
         return True
-    request = Request(config.get(server, 'auth_url'))
-    request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)')
-    request.add_header('cookie', 'auth._token.local='+token)
-    response = json.load(urlopen(request))
-    # logging.info('username: {username}, server: {server}, token: {token}')
-    return str(response['user']['id']) == username or response['user']['email'] == username + '@' + server
+    try:
+        request = Request(config.get(server, 'auth_url'))
+        request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)')
+        request.add_header('cookie', 'auth._token.local='+token)
+        response = json.load(urlopen(request))
+        # logging.info('username: {username}, server: {server}, token: {token}')
+        return str(response['user']['id']) == username or response['user']['email'] == username + '@' + server
+    except:
+        return False
 
 
 # print(do_auth(['info', 'aayulogic.com', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjYzNzg4MTA1LCJpYXQiOjE2NjM3NjY1MDUsImp0aSI6IjVjYmNlNzEyNjk3NDRkODFiM2RkZDE1YmM5ZDMxOGYxIiwidXNlcl9pZCI6MX0.IjbettQ5XIG1HZyzehjMuJoRML3c3kQPeQyPcherj3r7DLvgMdESuIO25Gsi30whsMd8FwnUiXdl5uo5Lwco-DP8n6zA-luVkqucbYftfutT9xbi1pkQ8jUcL9mJAHiGDFpYnCMshwfTzp3Sw-t2YBuOIMwgudhSXGenzRC2H-lm98zFLt5CTVFGRqN9GPcAFcHcOQPA_pQC8HoYx0imRlu7rGxcHqZxF0XRmBYBJ2seCA0rkP4YWV75-8Aqv3H4yKrWkyOnPQX3VP2MYJw7eUNAadNC1eNRAGn8TIyhZzAgR_7M-06RlhaZi6gsI7Y8faAEGnIBaPQ8LxmTk5A43w']))
